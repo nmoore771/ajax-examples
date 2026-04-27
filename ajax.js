b04e454276411af95084276465c783bffbf527a0
NCC Moore <60438265+nmoore771@users.noreply.github.com>
2026-04-27 14:25:22 -0400

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -4,15 +4,26 @@ function request1 () {
         .then(resp => resp.text())
         .then(data => {
             console.log(data);
-        })
+        });
 }
 
+// Asynchronous Communication is Asynchronous!  This does not always return values in the order you expect! 
+fucnction request2 () {
+    fetch("hello.txt")
+      .then(resp => resp.text())
+      .then(data => console.log(data));
 
-function request2 () {
+    fetch("goodbye.txt")
+      .then(resp => resp.text())
+      .then(data => console.log(data));
+}
+
+// Decoding JSON data
+function request3 () {
     let word = document.querySelector("#wordbox").value;
     fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + word)
         .then(resp => resp.json())
         .then(data => {
             console.log(data);
-        })
+        });
 }
