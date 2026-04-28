b440157239831a8f50df3914fe9f5592f7e36130
Nick Moore <nicholas.moore1@mohawkcollege.ca>
2026-04-28 16:07:42 -0400

    Added new request type

--- ajax.js
+++ ajax.js
@@ -1,5 +1,5 @@
 // baby's first AJAX request! Fetches contents of a text document.
-function request1 () {
+function request1() {
     fetch("hello.txt")
         .then(resp => resp.text())
         .then(data => {
@@ -7,23 +7,53 @@ function request1 () {
         });
 }
 
-// Asynchronous Communication is Asynchronous!  This does not always return values in the order you expect! 
-function request2 () {
+// Asynchronous Communication is Asynchronous!  This does not always return values in the order you expect!
+function request2() {
     fetch("hello.txt")
-      .then(resp => resp.text())
-      .then(data => console.log(data));
+        .then(resp => resp.text())
+        .then(data => console.log(data));
 
     fetch("goodbye.txt")
-      .then(resp => resp.text())
-      .then(data => console.log(data));
+        .then(resp => resp.text())
+        .then(data => console.log(data));
+}
+
+function request3() {
+    let word = document.querySelector("#wordbox").value;
+    fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + word)
+        .then(resp => resp.text())
+        .then(data => {
+            console.log(data);
+        });
 }
 
 // Decoding JSON data
-function request3 () {
+function request4() {
     let word = document.querySelector("#wordbox").value;
     fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + word)
         .then(resp => resp.json())
         .then(data => {
             console.log(data);
         });
+}
+
+// sending JSON data
+function request5() {
+    const obj =
+        {
+            id: 1,
+            title: "A Summer's Day",
+            body: "It was hot",
+            userID: 7,
+        };
+    fetch("https://jsonplaceholder.typicode.com/posts/1"
+        , {
+              method: "POST",
+              body: JSON.stringify(obj),
+              headers: {
+                'Content-type': 'application/json'
+            }
+          })
+        .then(resp => resp.json())
+        .then(data => console.log(data))
 }
